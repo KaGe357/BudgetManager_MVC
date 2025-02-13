7cc89d259a7394e43b34338ff27cd3699dd193e3
Kamil Gedaj <kamil.gedaj.programista@gmail.com>
2025-02-13 08:34:54 +0100

    minor change

--- src/App/Database/DatabaseInitializer.php
+++ src/App/Database/DatabaseInitializer.php
@@ -25,17 +25,5 @@ class DatabaseInitializer
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )
         ");
-
-        // Tworzenie tabeli `transactions`
-        $this->pdo->exec("
-            CREATE TABLE IF NOT EXISTS transactions (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                user_id INT NOT NULL,
-                amount DECIMAL(10,2) NOT NULL,
-                category VARCHAR(100) NOT NULL,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-            )
-        ");
     }
 }
